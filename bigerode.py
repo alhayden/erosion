dc8963b102886b93acf79945bcedc80f4046e3b5
Alexander Hayden <alexhayden25@gmail.com>
2019-04-13 15:58:45 -0400

    Not much

--- bigerode.py
+++ bigerode.py
@@ -1,7 +1,16 @@
 #!/bin/python
 
+
 from random import random, randint
 from outputs import *
 from matxutils import *
 
 
+def buildField(matx):
+    volume = makerix(len(matx) - 1)
+    velocity = [[ [0, 0, 0] for x in range(len(matx) - 1)] for y in range(len(matx) - 1)]
+
+
+if __name__ == "__main__":
+    m = makerix(8)
+    buildField(m)
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Erosion
 
+Realistic procedural terrain generation produced by simulating erosion.
 
+Inspired by [this video](https://www.youtube.com/watch?v=eaXk97ujbPQ&app=desktop)
